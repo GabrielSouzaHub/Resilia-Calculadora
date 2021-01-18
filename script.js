ad806e7802e9bea23abd988172101f4dc64e867a
Felipe Toledo <felip_elima@hotmail.com>
2021-01-18 17:19:29 -0300

    Adicionando função de limpar campos e alteração do CSS do botao C

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let operatorInput = document.createElement('input');
 let resultButton = document.createElement('button');
 
 let plusOneButton = document.createElement('button');
-let sumButton = document.createElement('button');
+let cleanButton = document.createElement('button');
 
 resultDiv.classList.add('result');
 operatorInput.classList.add('operatorInput');
@@ -17,7 +17,7 @@ numberOneInput.classList.add('numberInput');
 numberTwoInput.classList.add('numberInput');
 resultButton.classList.add('resultButton');
 plusOneButton.classList.add('plusOneButton');
-sumButton.classList.add('sumButton');
+cleanButton.classList.add('cleanButton');
 
 
 calculatorDiv.appendChild(numberOneInput);
@@ -25,60 +25,67 @@ calculatorDiv.appendChild(operatorInput);
 calculatorDiv.appendChild(numberTwoInput);
 calculatorDiv.appendChild(resultButton);
 calculatorDiv.appendChild(plusOneButton);
-calculatorDiv.appendChild(sumButton);
+calculatorDiv.appendChild(cleanButton);
 
 calculatorSection.appendChild(resultDiv);
 calculatorSection.appendChild(historicDiv);
 
 resultButton.textContent = 'Calculate';
-sumButton.textContent = '+';
+cleanButton.textContent = 'C';
 plusOneButton.textContent = '+1';
 
 let calculator = {
-    calculo : function(){
+    calculo: function () {
         let valueOne = parseInt(numberOneInput.value);
         let valueTwo = parseInt(numberTwoInput.value);
-    
-        resultDiv.innerHTML = "";
+
+        cleanPage();
 
         let result = document.createElement('p');
-        
+
         result.classList.add('result');
-        
+
         resultDiv.appendChild(result);
-        
+
         let operator = operatorInput.value;
-    
+
         if (operator == "+") {
-    
+
             let resultCase = valueOne + valueTwo;
             result.textContent = "Resultado: " + resultCase;
 
         } else if (operator == "-") {
-    
+
             let resultCase = valueOne - valueTwo;
             result.textContent = "Resultado: " + resultCase;
 
         } else if (operator == "/") {
-    
+
             let resultCase = valueOne / valueTwo;
             result.textContent = "Resultado: " + resultCase;
 
         } else if (operator == "*") {
-    
+
             let resultCase = valueOne * valueTwo;
             result.textContent = "Resultado: " + resultCase;
-        
+
         } else {
 
             result.textContent = "Operador inválido";
         }
-    
+
     }
 }
 
-function historic(){
-    
+function cleanPage() {
+    resultDiv.innerHTML = "";
 }
 
-resultButton.addEventListener ('click', calculator.calculo);
+
+resultButton.addEventListener('click', calculator.calculo);
+cleanButton.addEventListener('click', cleanPage);
+cleanButton.addEventListener('click', function () {
+    numberOneInput.value = '';
+    numberTwoInput.value = '';
+    operatorInput.value = '';
+})
